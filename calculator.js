92d4dbd6b447ad0780cc73895f7f8626853f5ffb
Peter Mowen <peter.m.mowen@gmail.com>
2026-01-14 20:06:26 -0500

    Add calculator object
    This object will be the core of the calculator. The UI will update
    term1, term2, and operator and call operate at appropriate times.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+let calculator = {
+  term1: 0,
+  term2: 0,
+  operator: "",
+  operate: function() {
+    switch (this.operator) {
+      case "+":
+        return this.term1 + this.term2;
+      case "-":
+        return this.term1 - this.term2;
+      case "/":
+        if (0 === this.term2) {
+          return NaN;
+        } else {
+          return this.term1 / this.term2;
+        }
+      case "*":
+        return this.term1 * this.term2;
+      default:
+        console.log(`unknown operator: ${this.operator}`);
+        return undefined;
+    }
+  }
+}
