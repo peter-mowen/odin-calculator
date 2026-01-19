2bbb976f86354311a80385182705cd68f8b8adbd
Peter Mowen <peter.m.mowen@gmail.com>
2026-01-19 13:29:22 -0500

    Add init, clearAll, and processNumberKeyPress
    init function gets the display-text element from the html document and
    initializes the displayedNumber to be the text content of this element.
    
    clearAll puts 0 back on the display and clears all internal variables.
    
    processNumberKeyPress builds the number on the screen based on whether
    or not we're starting a new number.
    
    At this point, you can do things like:
    - press key 1, display shows 1
    - press key 8, display shows 18
    - press AC, the display shows 0

--- calculator.js
+++ calculator.js
@@ -1,5 +1,35 @@
 let calculator = {
+  displayTextElement: null,
+  displayedNumber: null,
+  lastNumberSeen: null,
+  lastOperator: null,
+  startNewNumber: true,
+
+  init: function() {
+    this.displayTextElement = document.querySelector(".display-text");
+    this.displayedNumber = Number(this.displayTextElement.textContent);
+  },
+
+  clearAll: function() {
+    this.displayTextElement.textContent = 0;
+    this.displayedNumber = Number(this.displayTextElement.textContent);
+    this.lastNumberSeen = null;
+    this.lastOperator = null;
+    this.startNewNumber = true;
+  },
+
+  processNumberKeyPress: function(digit) {
+    if (this.startNewNumber) {
+      this.displayTextElement.textContent = digit;
+      this.startNewNumber = false;
+    } else {
+      this.displayTextElement.textContent = this.displayTextElement.textContent.concat(digit);
+    }
+    this.displayedNumber = Number(this.displayTextElement.textContent);
+  }
 }
+
+calculator.init();
 
 let numpad = document.querySelector(".numpad");
 
@@ -7,9 +37,11 @@ numpad.addEventListener('click', (event) => {
   switch(event.target.dataset.type) {
     case "clear":
       console.log(`AC`);
+      calculator.clearAll();
       break;
     case "number":
       console.log(`number key: ${event.target.dataset.value}`);
+      calculator.processNumberKeyPress(event.target.dataset.value);
       break;
     case "operator":
       console.log(`${event.target.dataset.value} operator selected`);
